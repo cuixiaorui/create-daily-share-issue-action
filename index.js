b81d89c31c1efbc60d6c5705be1cc022e07ddef6
cuixiaorui <cui_xiaorui@126.com>
2021-10-23 10:28:23 +0800

    fix date not is chine time

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const dayjs = require("dayjs");
 
 (function main() {
   const token = core.getInput("token");
@@ -22,19 +23,12 @@ function getBody() {
 }
 
 function getTitle() {
-  return `【学习日报】 ${getDate()}`;
+  return `【每日计划】 ${getDate()}`;
 }
 
 function getDate() {
-  const date = new Date();
-  const year = date.getFullYear();
-
-  const month = normalizeDate(date.getMonth() + 1);
-  const day = normalizeDate(date.getDate());
-
-  return `${year}-${month}-${day}`;
-}
-
-function normalizeDate(n) {
-  return n < 10 ? `0${n}` : n;
+  // 运行环境是 UTC 时区
+  // 需要转换成 中国时区
+  // 中国时区 = UTC时区 + 8小时
+  return dayjs().add("8", "hour").format("YYYY-MM-DD");
 }
